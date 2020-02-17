ea4f3bc6d57bee6a0aa904cdb3e2bb5762961774
andreaszafi <60961295+andreaszafi@users.noreply.github.com>
2020-02-17 17:13:08 +0100

    unit test integration

--- unit_tests.py
+++ unit_tests.py
@@ -1,7 +1,18 @@
 from numericaltools import integrate
-
-x = 1
+import unittest
 
 def func(x):
     y = 2*x + 1
-print(integrate(func, 0, 6, 7))
+    return y
+
+print(integrate(func, 0, 6, 3))
+
+class SimpleTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def test_integrate(self):
+        self.assertEqual( integrate(func, 0, 6, 3), 42)
+
+if __name__ == '__main__':
+    unittest.main()
