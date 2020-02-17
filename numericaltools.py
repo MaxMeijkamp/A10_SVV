bac23482e680ff6c731277eae1a1fc1f3cb361e0
Nick Eichman Baquer <nick.eichman@gmail.com>
2020-02-18 00:13:56 +0100

    Integration cleaned, spline initiated

--- numericaltools.py
+++ numericaltools.py
@@ -1,28 +1,27 @@
 import numpy as np
 
-#Integration function, with func being the (mathematical) function to integrate, start and stop being beginning and end
-#of the integration respectively, and number_of_points the subdivisions.
+
+# Integration function, with func being the (mathematical) function to integrate, start and stop being beginning and end
+# of the integration respectively, and number_of_points the subdivisions.
 def integrate(func, start, stop, number_of_points):
-    _integration_temp = 0
-    _diff = (start+stop)/number_of_points
+    integration_temp = 0
+    diff = (start+stop) / number_of_points
     for N in range(0, number_of_points):
-        _point_a = start+N*_diff
-        _point_b = _point_a+_diff
-        _integration_temp += _diff/2*(func(_point_a)+func(_point_b))
-    integration = _integration_temp
+        point_a = start + N*diff
+        point_b = point_a + diff
+        integration_temp += diff / 2 * (func(point_a) + func(point_b))
+    integration = integration_temp
     return integration
 
 
-# function which calculates the value of each section in the linear spline,
-# also can be referenced as a spline item.
-def splineitem(fi, f1, xi, x1, x):
-    #
-    splinei = fi + ((f1-fi)/(x1-xi))*(x-xi)
-    return splinei
-
-
-def spline(x):
-    #   Write code which for every range x-xi calls upon the splineitem function
-    #   to build a linear spline.
-    sp = 1 * x
+# Spline function,
+def spline(x, f, n):
+    sp_start = []
+    sp_slope = []
+    for i in range(0,n):
+        splinestart = f[i] + ((f[i+1] - f[i])/(x[i+1] - x[i]))*x[i]
+        splineslope = ((f[i+1] - f[i])/(x[i+1] - x[i]))
+        sp_start.append(splinestart)
+        sp_slope.append(splineslope)
+    sp = np.empty(shape=[n, 2])
     return sp
