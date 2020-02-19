184b5719a532f4db9c5dedd875c16c15ad3d038c
Paco Frantzen <paco.frantzen@ziggo.nl>
2020-02-19 11:40:26 +0100

    started on Shear center function

--- stress_modules.py
+++ stress_modules.py
@@ -1,5 +1,6 @@
 import numpy as np
 import geometry
+import numericaltools
 
 def von_mises(sigma_xx, sigma_yy, sigma_zz, tau_xy, tau_yz, tau_xz):
     #Takes arg. sigma_xx, sigma_yy, sigma_zz, tau_xy, tau_yz, tau_xz.
@@ -11,5 +12,17 @@ def bending(y, z, Iyy, Izz, M_y, M_z, centroid):
     #Cont'd: the MOI Izz, Iyy; the relevant moments M_z, M_y (tension in + plane positive), centroid: tuple with xyz centroid of the aileron
     return M_y *  (z - centroid[2]) / Iyy + M_z * (y - centroid[1]) / Izz
 
-def shear():
-    return
+def findShearCenter(dataset):
+    Izz = dataset.Izz()
+    SC_y = dataset.centroid[1]
+    Vy = 1 #force applied in shear centre
+    #cuts at front of aileron and middle in spar
+    r = dataset.height * .5
+    dataset.a
+    def finddqb(func, s, Izz):
+        dqb = -1. / Izz * (numericaltools.integrate(func, 0, s, 100000)
+
+        return
+    return Izz
+
+def torsion(y, z, torque, dataset)
