6f1043e26e9ddada0538d599ab0c0552e907a0ec
andreaszafi <60961295+andreaszafi@users.noreply.github.com>
2020-02-20 15:45:20 +0100

    Merge branch 'master' of C:\Users\andreas\PycharmProjects\A10_SVV with conflicts.

--- tests.py
+++ tests.py
@@ -6,7 +6,6 @@ import numpy as np
 import InputClasses
 import displacements
 import equilibrium
-from InputClasses import *
 
 class MyTestCase(unittest.TestCase):
 
@@ -93,14 +92,13 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(stress_modules.von_mises(sigma_xx=.0000001, sigma_yy=0, sigma_zz=0, tau_xy=0, tau_yz=0, tau_xz=0), math.sqrt(.0000001**2))
 
     def test_integration(self):
-        # integrate(func, start, stop, number_of_points)
+        #integrate(func, start, stop, number_of_points)
         start = 0
         stop = 1
-
         def tempfunc(x):
             return 1 + x * 0
         n = 1
-        self.assertAlmostEqual(numericaltools.integrate(tempfunc, start, stop, n), 1., places=8)
+        self.assertAlmostEqual(numericaltools.integrate(tempfunc, start, stop, n), 1., places= 8)
         n =100
         self.assertAlmostEqual(numericaltools.integrate(tempfunc, start, stop, n), 1., places=8)
         start = -5
@@ -129,7 +127,7 @@ class MyTestCase(unittest.TestCase):
         # Very small distance over which to integrate
         self.assertAlmostEqual(numericaltools.integrate(tempfunc, start, stop, n), 0.00000001**2, places=10)
 
-        # Harder book example from Early Transcendentals
+        #More difficult book example from Early Transcendentals
         def tempfunc(x):
             return np.log(x) / x
         start = 1
@@ -157,33 +155,21 @@ class MyTestCase(unittest.TestCase):
         test_x_target = 11
         self.assertEqual(numericaltools.interpolate(test_list_x_1, test_list_f_1, test_x_target), 1)
 
-    def test_input_geo(self):
-        # Tests Izz
-        chord = 1.1
-        height = 0.8
-        skint = 0.01
-        spart = 0.05
-        a = Aileron(chord=chord, height=height, skint=skint, spart=spart)
-        # self.assertEqual(a.Izz(stiffener=False, spar=False), 0.001865283305) # Exact value different from
-        # manually calculated value by less than 0.1%, hence human error and can be interpreted as correct
-        # self.assertEqual(a.Izz(skin=False, spar=False),)
-        self.assertEqual(a.Izz(skin=False, stiffener=False), 0.0256/12)
+class SystemTests(unittest.TestCase):
+    def test_no_load_no_deformation(self):
 
-        # Tests Iyy
+    def test_unit_loads_applied(self):
 
-        # Tests centroid
+    def test_normal_loads_compared_with_hand_calculations_at_different_points(self):
 
-        # Tests shearcentre
-        # TODO: implement in InputClasses.py
+    def test_check_hinge_2_deflection_0_at_different_loads(self):
+        self.assertIsNone(displacements.displ(normal loads))
 
-        # Tests visualinspection?
-
-        # Tests _Istiff
-
-
-# class SystemTests(unittest.TestCase):
-#     def test_no_load_no_deformation(self):
-#  TODO: CHANGE THIS YO, SHOULDN'T BE COMMENTED
+    def test_check_large_E_G_give_small_displacements(self):
+        self.assertLess(displacements.displ(normal loads, large E), some low displacement value,))
+        self.assertLess(displacements.displ(normal loads, very very large E), some super low displacement value,))
+        self.assertLess(displacements.angle_displ(normal loads, large G), some low angle value,))
+        self.assertLess(displacements.angle_displ(normal loads, very very large G), some suprt low angle value,))
 
 if __name__ == '__main__':
     data = InputClasses.Aileron()
