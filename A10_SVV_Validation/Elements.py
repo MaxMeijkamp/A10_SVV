29116e96292e5b83e2fb14a1743405bfd6fa1a43
mustafawahid <mustie-5@hotmail.com>
2020-02-23 21:14:42 +0100

    hallo

--- A10_SVV_Validation/Elements.py
+++ A10_SVV_Validation/Elements.py
@@ -8,11 +8,4 @@ def elements(n):
         if int(i[0])==n:
             return i[1],i[2],i[3],i[4]
 
-totalstresslist_bending = np.genfromtxt("B737.rpt", dtype=str, skip_header=20, skip_footer=(59956 - 5799), delimiter=",")
-totalstresslist_bending = totalstresslist_bending.astype(np.float)
-
-totalstresslist_jam_bent = np.genfromtxt("B737.rpt", dtype=str, skip_header=6705, skip_footer=(59956 - ), delimiter=",")
-totalstresslist_jam_bent = totalstresslist_jam_bent.astype(np.float)
-
-totalstresslist_jam_straight = np.genfromtxt("B737.rpt", dtype=str, skip_header=9, skip_footer=(59956 - 6598), delimiter=",")
-totalstresslist_jam_straight = totalstresslist_jam_straight.astype(np.float)
+print(elements(600))
---
+++ A10_SVV_Validation/Max_stresses.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+
+totalstresslist_bending = np.genfromtxt("B737.rpt", dtype=str, skip_header=20, skip_footer=(59956 - 5799-165))
+totalstresslist_jam_bent = np.genfromtxt("B737.rpt", dtype=str, skip_header=6705, skip_footer=(59956 - 12484-146))
+totalstresslist_jam_straight = np.genfromtxt("B737.rpt", dtype=str, skip_header=13390, skip_footer=(59956 - 19169-127))
+
+
+von_misses_bending=[]
+# print(int(totalstresslist_bending[0][2])+int(totalstresslist_bending[0][2]))
+for i in totalstresslist_bending:
+    von_misses_bending.append((float(i[2]) + float(i[3]))*0.5)
+print (max(von_misses_bending))
+
+von_misses_jam_straight=[]
+von_misses_jam_bent=[]
+s12_bending=[]
+s12_jam_bent=[]
+s12_jam_straight=[]
+
