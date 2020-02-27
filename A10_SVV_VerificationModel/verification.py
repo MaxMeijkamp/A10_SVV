9575ac0d4ef3343142fcd07e030e6a7c42734410
andreaszafi <60961295+andreaszafi@users.noreply.github.com>
2020-02-27 14:10:25 +0100

    Writing system tests, checking deformation at hinge of verification model

--- A10_SVV_VerificationModel/verification.py
+++ A10_SVV_VerificationModel/verification.py
@@ -33,20 +33,39 @@ class MyTestCase(unittest.TestCase):
 
 #    def test_stiff_s_pos(self):
 
-class SystemTests(unittest.TestCase):
-    def test_no_load_no_deformation(self):
-        self.assertIsNone(equilibrium.calc_deflection_y(loads=0, u = np.nan))
-
+#class SystemTests(unittest.TestCase):
+#    def test_no_load_no_deformation(self):
+#        self.assertIsNone(equilibrium.calc_deflection_y(loads=0, u = np.nan))
+#        self.assertIsNone(equilibrium.calc_deflection_z(loads=0, w = np.nan))
+#
 #    def test_unit_loads_applied(self):
+#        self.assertAlmostEqual(equilibrium.calc_deflection_y(loads=1, u = np.nan))
+#        self.assertAlmostEqual(equilibrium.calc_deflection_z(loads=1, w = np.nan))
 #
 #    def test_normal_loads_compared_with_hand_calculations_at_different_points(self):
+#        self.assertAlmostEqual(equilibrium.calc_deflection_y(loads=np.nan, u=np.nan))
+#        self.assertAlmostEqual(equilibrium.calc_deflection_y(loads=np.nan, u=np.nan))
+#        self.assertAlmostEqual(equilibrium.calc_deflection_y(loads=np.nan, u=np.nan))
+#
+#        self.assertAlmostEqual(equilibrium.calc_deflection_z(loads=np.nan, w=np.nan))
+#        self.assertAlmostEqual(equilibrium.calc_deflection_z(loads=np.nan, w=np.nan))
+#        self.assertAlmostEqual(equilibrium.calc_deflection_z(loads=np.nan, w=np.nan))
 #
 #    def test_check_hinge_2_deflection_0_at_different_loads(self):
-#         self.assertIsNone(displacements.displ(normal loads))
+#         self.assertIsNone(displacements.displ(some load))
+#         self.assertIsNone(displacements.displ(some load))
+#         self.assertIsNone(displacements.displ(some load))
+#         self.assertIsNone(displacements.displ(some load))
 #
 #    def test_check_large_E_G_give_small_displacements(self):
+#        Elarge =
+#        Evlarge =
+#        Glarge =
+#        Gvlarge =
+#
 #         self.assertLess(displacements.displ(normal loads, large E), some low displacement value,))
 #         self.assertLess(displacements.displ(normal loads, very very large E), some super low displacement value,))
+#
 #         self.assertLess(displacements.angle_displ(normal loads, large G), some low angle value,))
 #         self.assertLess(displacements.angle_displ(normal loads, very very large G), some suprt low angle value,))
 
@@ -54,8 +73,8 @@ class SystemTests(unittest.TestCase):
 if __name__ == '__main__':
 #    print(mainfile.Izz, mainfile.Iyy)
 #    print(main.d1, main.e1)
-    print(mainfile.stcoord)
-    print("aaa",np.array(aileron.stiffLoc(3)))
+#    print(mainfile.stcoord)
+#    print("aaa",np.array(aileron.stiffLoc(3)))
 
     unittest.main()
 
