d84dc2836baee61baeb79c6324221299c4ceb6eb
mustafawahid <mustie-5@hotmail.com>
2020-02-28 09:49:23 +0100

    Update: nodes => elements elements => nodes finds all nodes of a crossection(needs correction) finds maximum stresses finds stresses of given crossection(needs correction) Update: correction fixed kind of

--- A10_SVV_Validation/Max_stresses.py
+++ A10_SVV_Validation/Max_stresses.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 Created on Tue Feb 25 15:23:36 2020
 
@@ -52,6 +53,15 @@ def get_dat(case, param):
     return (newload)
 
 
+def node_x(node):
+    # returns x-coordinate OF GIVEN NODE
+    nodelist = np.genfromtxt("B737.inp", dtype=str, skip_header=9, skip_footer=(14594 - 6598), delimiter=",")
+    nodelist = nodelist.astype(np.float)
+    for i in nodelist:
+        if int(i[0]) == node:
+            return i[1]
+
+
 def nodes(element):
     # enter element number and gives corresponding nodes
     elementlist = np.genfromtxt("B737.inp", dtype=str, skip_header=6598, skip_footer=(14594 - 13233), delimiter=",")
@@ -78,20 +88,53 @@ def elements(node):
     return elements
 
 
+def integrationpoint(element):
+    elementlist = np.genfromtxt("B737.inp", dtype=str, skip_header=6598, skip_footer=(14594 - 13233), delimiter=",")
+    elementlist = elementlist.astype(np.float)
+    allnodes = nodes(element)
+    for i in elementlist:
+        if int(i[0]) == element:
+            integrationpoint_x = (node_x(int(max(allnodes))) + node_x(int(min(allnodes)))) * 0.5
+            return integrationpoint_x
+
+
 # fix this part
-def crossection(element):
+def crossection(node):
     # finds all the nodes in the crossection of given node
     elementlist = np.genfromtxt("B737.inp", dtype=str, skip_header=9, skip_footer=(14594 - 6598), delimiter=",")
     elementlist = elementlist.astype(np.float)
     crossection_list = []
 
     for i in elementlist:
-        if int(i[0]) == element:  # find line with corresponding element
+        if int(i[0]) == node:  # find line with corresponding element
             for n in elementlist:
                 if int(n[1]) == int(i[1]):  # finds all the elements with the same x coordinate as the input element
                     crossection_list.append(int(n[0]))
 
-    return crossection_list
+    return np.array(crossection_list)
+
+
+def crossection_element(element):
+    a1 = crossection(nodes(element)[0])
+    a2 = crossection(nodes(element)[2])
+    a = list(np.hstack((a1, a2)))
+
+    lst = []  # list of all the nodes of the crossection of nodes on the left and right of the element
+    for i in a:
+        lst = lst + elements(i)
+    crossectionlist = []
+    for i in lst:
+        if lst.count(i) == 4:
+            if crossectionlist.count(i) == 0:
+                crossectionlist.append(i)
+        if lst.count(i) == 6:
+            if crossectionlist.count(i) == 0:
+                crossectionlist.append(i)
+
+    return np.sort(crossectionlist)
+
+
+# nodeslist = np.genfromtxt("B737.inp", dtype=str, skip_header=9, skip_footer=(14594 - 6598), delimiter=",")
 
 
 def max_von_mises(case):
@@ -127,7 +170,7 @@ def s12(case, element):
 
 def crossection_von_mises(case):
     a = max_von_mises(case)[1]
-    b = crossection(a)
+    b = crossection_element(a)
     crossection_von_mises_list = []
 
     for i in b:
@@ -138,10 +181,128 @@ def crossection_von_mises(case):
 
 def crossection_s12(case):
     a = max_s12(case)[1]
-    b = crossection(a)
+    b = crossection_element(a)
     crossection_s12_list = []
 
     for i in b:
         crossection_s12_list.append(s12(case, i))
 
     return crossection_s12_list
+
+
+print (crossection_element(max_von_mises('bending')[1]))
+print (crossection_von_mises('bending'))
+
+# element=5190
+# nodes_list=nodes(element)
+# crossection1 = crossection(min(nodes(element)))
+# crossection2 = crossection(max(nodes(element)))
+# crossectionlist_nodes=crossection1+crossection2
+# crossectionlist=[]
+# for i in range (6000):
+#    nodes_list=nodes(i)
+#    print(nodes_list)
+
+
+# def crossection_element(element):
+#    crossection1 = crossection(min(nodes(element)))
+#    crossection2 = crossection(max(nodes(element)))
+#    crossectionlist_nodes=crossection1+crossection2
+#    crossectionlist=[]
+#    for i  in  range(1,max(crossectionlist_nodes)+1):
+#        if crossectionlist_nodes.count(nodes(i)[0])+crossectionlist_nodes.count(nodes(i)[1])+crossectionlist_nodes.count(nodes(i)[2])+crossectionlist_nodes.count(nodes(i)[3])=4:
+#            crossectionlist.append(i)
+#    return crossectionlist
+#
+# print(crossection_element(5190))
+
+#    a = nodes(i)
+#    if crossectionlist_nodes.count(a[0])+crossectionlist_nodes.count(a[1])+crossectionlist_nodes.count(a[2])+crossectionlist_nodes.count(a[3])=4:
+#        crossectionlist.append(i)
+# print(crossectionlist)
+
+
+# def crossection_elements(element):
+#    crossection1 = crossection(min(nodes(element)))
+#    crossection2 = crossection(max(nodes(element)))
+#    
+#    crossectionlist_nodes=crossection1+crossection2
+#    crossectionlist = []
+
+# print (nodes(5190))
+
+
+# import matplotlib.pyplot as plt
+#
+## input file
+# nodelist = np.genfromtxt("B737.inp", dtype=str, skip_header=9, skip_footer=(14594 - 6598), delimiter=",")
+# nodelist = nodelist.astype(np.float)
+#
+# hingelinenodes = []
+# hingelinenodes_x_coordinate = []
+# for i in nodelist:
+#    if i[2] == 0:
+#        if i[3] == 0:
+#            hingelinenodes.append(i[0])
+#            hingelinenodes_x_coordinate.append(i[1])
+#
+## Deflection data - loading case: bending
+# c = get_dat('bending','disp')
+# c = c.astype(np.float)
+# hingelinedata_bend = []
+# x_bend = []
+# y_bend = []
+# z_bend = []
+# mag_bend = []
+# print(c)
+## Deflection data - loading case: jam_bent
+# d = get_dat('Jam_Bent','disp')
+# d = d.astype(np.float)
+# hingelinedata_jam_bent = []
+# x_jam_bent = []
+# y_jam_bent = []
+# z_jam_bent = []
+# mag_jam_bent = []
+# print(d)
+## Deflection data - loading case: jam_bent
+# e = get_dat('Jam_Straight','disp')
+# e = e.astype(np.float)
+# hingelinedata_jam_straight = []
+# x_jam_straight = []
+# y_jam_straight = []
+# z_jam_straight = []
+# mag_jam_straight = []
+# print(e)
+## Find hingeline data for loading cases
+# s=0
+# for i in hingelinenodes:
+#    i = int(i) - 1
+#    hingelinedata_bend.append(c[i])
+#    mag_bend.append(c[i][1])
+#    x_bend.append(c[i][2]+hingelinenodes_x_coordinate[s])
+#    y_bend.append(c[i][3])
+#    z_bend.append(c[i][4])
+#
+#    hingelinedata_jam_bent.append(d[i])
+#    mag_jam_bent.append(d[i][1])
+#    x_jam_bent.append(d[i][2]+hingelinenodes_x_coordinate[s])
+#    y_jam_bent.append(d[i][3])
+#    z_jam_bent.append(d[i][4])
+#
+#    hingelinedata_jam_straight.append(e[i])
+#    mag_jam_straight.append(e[i][1])
+#    x_jam_straight.append(e[i][2]+hingelinenodes_x_coordinate[s])
+#    y_jam_straight.append(e[i][3])
+#    z_jam_straight.append(e[i][4])
+#
+#    s=s+1
+#
+# plt.title('Vertical Hingeline deflection x-y plane')
+# plt.xlabel('Location x-axis[mm]')
+# plt.ylabel('Vertical deflection [mm]')
+# plt.plot(x_bend, y_bend, 'bo') #bluedots
+# plt.plot(x_jam_bent, y_jam_bent, 'ro') #reddots
+# plt.plot(x_jam_straight, y_jam_straight, 'go') #greendots
+# plt.show()
+
+
