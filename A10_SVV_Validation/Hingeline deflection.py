66f0ab77e46a86f7bf5b4ea55689b2e860f9057c
mustafawahid <mustie-5@hotmail.com>
2020-02-28 10:16:25 +0100

    Update: nodes => elements elements => nodes finds all nodes of a crossection(needs correction) finds maximum stresses finds stresses of given crossection(needs correction) Update: correction fixed

--- A10_SVV_Validation/Hingeline deflection.py
+++ A10_SVV_Validation/Hingeline deflection.py
@@ -6,25 +6,23 @@ Created on Thu Feb 13 13:56:00 2020
 @author: mustafawahid
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 # input file
-a = np.genfromtxt("B737.inp", dtype=str, skip_header=9, skip_footer=(14594 - 6598), delimiter=",")
-a = a.astype(np.float)
-
+nodelist = np.genfromtxt("B737.inp", dtype=str, skip_header=9, skip_footer=(14594 - 6598), delimiter=",")
+nodelist = nodelist.astype(np.float)
 
 hingelinenodes = []
-hingelinenodes_x = []
-for i in a:
+hingelinenodes_x_coordinate = []
+for i in nodelist:
     if i[2] == 0:
         if i[3] == 0:
             hingelinenodes.append(i[0])
-            hingelinenodes_x.append(i[1])
+            hingelinenodes_x_coordinate.append(i[1])
 
-print(hingelinenodes)
 # Deflection data - loading case: bending
-c = np.genfromtxt("B737.rpt", skip_header=20074, skip_footer=(59956 - 26662 - 109))
+c = get_dat('bending', 'disp')
 c = c.astype(np.float)
 hingelinedata_bend = []
 x_bend = []
@@ -33,7 +31,7 @@ z_bend = []
 mag_bend = []
 
 # Deflection data - loading case: jam_bent
-d = np.genfromtxt("B737.rpt", skip_header=26724, skip_footer=(59956 - 33313 - 89))
+d = get_dat('Jam_Bent', 'disp')
 d = d.astype(np.float)
 hingelinedata_jam_bent = []
 x_jam_bent = []
@@ -42,7 +40,7 @@ z_jam_bent = []
 mag_jam_bent = []
 
 # Deflection data - loading case: jam_bent
-e = np.genfromtxt("B737.rpt", skip_header=33374, skip_footer=(59956 - 39963 - 70))
+e = get_dat('Jam_Straight', 'disp')
 e = e.astype(np.float)
 hingelinedata_jam_straight = []
 x_jam_straight = []
@@ -51,37 +49,33 @@ z_jam_straight = []
 mag_jam_straight = []
 
 # Find hingeline data for loading cases
-s=0
+s = 0
 for i in hingelinenodes:
     i = int(i) - 1
-    print(hingelinenodes_x[s])
     hingelinedata_bend.append(c[i])
     mag_bend.append(c[i][1])
-    x_bend.append(c[i][2]+hingelinenodes_x[s])
+    x_bend.append(c[i][2] + hingelinenodes_x_coordinate[s])
     y_bend.append(c[i][3])
     z_bend.append(c[i][4])
 
     hingelinedata_jam_bent.append(d[i])
     mag_jam_bent.append(d[i][1])
-    x_jam_bent.append(d[i][2]+hingelinenodes_x[s])
+    x_jam_bent.append(d[i][2] + hingelinenodes_x_coordinate[s])
     y_jam_bent.append(d[i][3])
     z_jam_bent.append(d[i][4])
 
     hingelinedata_jam_straight.append(e[i])
     mag_jam_straight.append(e[i][1])
-    x_jam_straight.append(e[i][2]+hingelinenodes_x[s])
+    x_jam_straight.append(e[i][2] + hingelinenodes_x_coordinate[s])
     y_jam_straight.append(e[i][3])
     z_jam_straight.append(e[i][4])
 
-    s=s+1
+    s = s + 1
 
-# plt.title('Vertical Hingeline deflection x-y plane')
-# plt.xlabel('Location x-axis[mm]')
-# plt.ylabel('Vertical deflection [mm]')
-plt.plot(x_bend, z_bend, 'bo') #bluedots
-# plt.plot(x_jam_bent, y_jam_bent, 'ro') #reddots
+plt.title('Vertical Hingeline deflection x-y plane')
+plt.xlabel('Location x-axis[mm]')
+plt.ylabel('Vertical deflection [mm]')
+plt.plot(x_bend, y_bend, 'bo') #bluedots
+plt.plot(x_jam_bent, y_jam_bent, 'ro') #reddots
 plt.plot(x_jam_straight, y_jam_straight, 'go') #greendots
 plt.show()
-
-
-
--- A10_SVV_Validation/Max_stresses.py
+++ A10_SVV_Validation/Max_stresses.py
@@ -142,78 +142,7 @@ def crossection_s12(case):
     return crossection_s12_list
 
 
-import matplotlib.pyplot as plt
 
-# input file
-nodelist = np.genfromtxt("B737.inp", dtype=str, skip_header=9, skip_footer=(14594 - 6598), delimiter=",")
-nodelist = nodelist.astype(np.float)
-
-hingelinenodes = []
-hingelinenodes_x_coordinate = []
-for i in nodelist:
-    if i[2] == 0:
-        if i[3] == 0:
-            hingelinenodes.append(i[0])
-            hingelinenodes_x_coordinate.append(i[1])
-
-# Deflection data - loading case: bending
-c = get_dat('bending', 'disp')
-c = c.astype(np.float)
-hingelinedata_bend = []
-x_bend = []
-y_bend = []
-z_bend = []
-mag_bend = []
-
-# Deflection data - loading case: jam_bent
-d = get_dat('Jam_Bent', 'disp')
-d = d.astype(np.float)
-hingelinedata_jam_bent = []
-x_jam_bent = []
-y_jam_bent = []
-z_jam_bent = []
-mag_jam_bent = []
-
-# Deflection data - loading case: jam_bent
-e = get_dat('Jam_Straight', 'disp')
-e = e.astype(np.float)
-hingelinedata_jam_straight = []
-x_jam_straight = []
-y_jam_straight = []
-z_jam_straight = []
-mag_jam_straight = []
-
-# Find hingeline data for loading cases
-s = 0
-for i in hingelinenodes:
-    i = int(i) - 1
-    hingelinedata_bend.append(c[i])
-    mag_bend.append(c[i][1])
-    x_bend.append(c[i][2] + hingelinenodes_x_coordinate[s])
-    y_bend.append(c[i][3])
-    z_bend.append(c[i][4])
-
-    hingelinedata_jam_bent.append(d[i])
-    mag_jam_bent.append(d[i][1])
-    x_jam_bent.append(d[i][2] + hingelinenodes_x_coordinate[s])
-    y_jam_bent.append(d[i][3])
-    z_jam_bent.append(d[i][4])
-
-    hingelinedata_jam_straight.append(e[i])
-    mag_jam_straight.append(e[i][1])
-    x_jam_straight.append(e[i][2] + hingelinenodes_x_coordinate[s])
-    y_jam_straight.append(e[i][3])
-    z_jam_straight.append(e[i][4])
-
-    s = s + 1
-
-# plt.title('Vertical Hingeline deflection x-y plane')
-# plt.xlabel('Location x-axis[mm]')
-# plt.ylabel('Vertical deflection [mm]')
-# plt.plot(y_bend, z_bend, 'bo') #bluedots
-# plt.plot(y_jam_bent, z_jam_bent, 'ro') #reddots
-# plt.plot(y_jam_straight, z_jam_straight, 'go') #greendots
-# plt.show()
 
 
 
